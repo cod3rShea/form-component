867db2aed7794d72e5e885c5c3ce362f347a1deb
Sean Michael Shea <seanshea1337@gmail.com>
2021-07-18 11:46:33 -0400

    intial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Forms from "./components/Forms/Forms";
+function App() {
+	return (
+		<div className="container">
+			<Forms />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/Forms/Forms.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+// JS
+// const input = document.getElementById('myText');
+// const inputValue = input.value
+// React
+// value, onChange
+
+const Forms = () => {
+	return (
+		<article>
+			<h2>Forms</h2>
+			<form className="form"></form>
+		</article>
+	);
+};
+
+export default Forms;
