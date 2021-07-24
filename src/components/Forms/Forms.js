ae9384d61fb7268ba79c98f5cfbf89d7a2dbd7da
Sean Michael Shea <seanshea1337@gmail.com>
2021-07-24 10:15:28 -0400

    add age input field and add sex dropdown input

--- src/components/Forms/Forms.js
+++ src/components/Forms/Forms.js
@@ -3,17 +3,25 @@ import React, { useState } from "react";
 const Forms = () => {
 	const [firstName, setFirstName] = useState("");
 	const [lastName, setLastName] = useState("");
+	const [age, setAge] = useState("");
+	const [sex, setSex] = useState("");
 	const [email, setEmail] = useState("");
 	const [people, setPeople] = useState([]);
 
 	console.log(people);
+	console.log(firstName);
+	console.log(lastName);
+	console.log(age);
+	console.log(sex);
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		if (firstName && lastName && email) {
+		if (firstName && lastName && age && sex && email) {
 			const person = {
 				firstName: firstName,
 				lastName: lastName,
+				age: age,
+				sex: sex,
 				email: email,
 			};
 
@@ -22,6 +30,8 @@ const Forms = () => {
 			});
 			setFirstName("");
 			setLastName("");
+			setAge("");
+			setSex("");
 			setEmail("");
 		} else {
 			console.log("enter value");
@@ -53,6 +63,30 @@ const Forms = () => {
 					/>
 				</div>
 				<div className="form-control">
+					<label htmlFor="age">Age:</label>
+					<input
+						type="text"
+						id="age"
+						name="age"
+						value={age}
+						onChange={(e) => setAge(e.target.value)}
+					/>
+				</div>
+				<div className="form-control">
+					<label htmlFor="sex">Sex:</label>
+					<select onChange={(e) => setSex(e.target.value)} id="sex">
+						<option value="" selected disabled hidden>
+							Select Sex
+						</option>
+						<option name="sex" value="Male">
+							Male
+						</option>
+						<option name="sex" value="Female">
+							Female
+						</option>
+					</select>
+				</div>
+				<div className="form-control">
 					<label htmlFor="email">Email:</label>
 					<input
 						type="email"
@@ -67,12 +101,14 @@ const Forms = () => {
 				</button>
 			</form>
 			{people.map((person) => {
-				const { firstName, lastName, email } = person;
+				const { firstName, lastName, age, sex, email } = person;
 				return (
 					<div className="item" key={email}>
 						<h4>
 							{firstName} {lastName}
 						</h4>
+						<span>{age}</span>
+						<span>{sex}</span>
 						<p>{email}</p>
 					</div>
 				);
